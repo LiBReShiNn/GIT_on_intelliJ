94f4ec9c14e1dee16678c8bdd53f83f49a5f1c57
LiBReShiNn <shinhj1991@gmail.com>
2019-06-30 23:29:28 +0900

    뭔가 충돌이 있어서 전부 합병하고 나서 지금 다시 올림. 가장 최근것 부터 거스르는 지점 까지 모든 충돌을 보여주나봄 신기하네

--- rain/src/Main.java
+++ rain/src/Main.java
@@ -23,8 +23,14 @@ public class Main {
         main3();
         Imp impl = new Imp();
         impl.run();
-        Imp impl2 = new Imp("z케호로리 호리리 호로돗 케케케",2);
+        Imp impl2 = new Imp("z케호로리 호리리 호로돗 케케케",222222);
         impl2.run();
+        Imp impl3 = new Imp("서브마스터 생성이라귯",3333333);
+        impl3.run();
+        Imp impl4 = new Imp("과거 히스토리에서 브랜치 생성 후 재수정 케케 ",444444);
+        impl4.run();
+        Imp impl5 = new Imp("충돌 해결하고 합병 햇다긔 지금 헤더는 여기라귯",55555);
+        impl5.run();
         impl.toString();
     }
 
